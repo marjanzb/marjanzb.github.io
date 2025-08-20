0bf0e486f5b99d592af098371eb4ab20e57a257f
marjanzb <126134993+marjanzb@users.noreply.github.com>
2025-08-20 04:14:03 +0000

    Deploying to gh-pages from @ marjanzb/marjanzb.github.io@c8ea248ced308aabf172cca23202439f460ebed1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,8 +419,8 @@ ninja.data = [{
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-mitigating-methane-emissions-from-active-and-abandoned-gas-wells",
+          title: 'Mitigating Methane Emissions from Active and Abandoned Gas Wells',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
